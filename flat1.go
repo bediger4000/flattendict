0bb0fc7758cb5bf9224fbcf8c21b499e5fe37ba2
Bruce Ediger <bediger8@gmail.com>
2020-12-22 20:40:45 -0700

    Semi-working version

--- flat1.go
+++ flat1.go
@@ -21,5 +21,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%v\n", x)
+	flatten("", x)
+}
+
+func flatten(hdr string, d interface{}) {
+	// fmt.Printf("%q: %T\n", hdr, d)
+	switch d.(type) {
+	case float64:
+		fmt.Printf("\t%q: %f,\n", hdr, d.(float64))
+	case map[string]interface{}:
+		if hdr != "" {
+			fmt.Printf("\t%q: ", hdr)
+		}
+		fmt.Println("{")
+		for name, interf := range d.(map[string]interface{}) {
+			newhdr := name
+			if hdr != "" {
+				newhdr = fmt.Sprintf("%s.%s", hdr, name)
+			}
+			flatten(newhdr, interf)
+		}
+		fmt.Println("}")
+	}
 }
